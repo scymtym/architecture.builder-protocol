00a4259e4ed127846b4ea941369aca9d8f4b1a43
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-08-21 10:51:41 +0200

    Test implicit and explicit function versions in test/protocol.lisp
    * test/protocol.lisp (with-implicit-and-explicit-builder): new macro;
      for testing FUNCTION and FUNCTION* variants of FUNCTION
      (prepare.smoke): use it
      (finish.smoke): likewise
      (wrap.smoke): likewise
      (make-node.smoke): likewise
      (relate.smoke): likewise

--- test/protocol.lisp
+++ test/protocol.lisp
@@ -8,19 +8,39 @@
 
 (in-suite :architecture.builder-protocol)
 
+;;; Utilities
+
+(defmacro with-implicit-and-explicit-builder
+    ((builder-var builder) explicit-function &body body)
+  (let ((implicit-function (symbolicate explicit-function '*)))
+    `(progn
+       ;; Explicit builder argument.
+       (let ((,builder-var ,builder)) ,@body)
+       ;; Implicit builder argument
+       (let* ((,builder-var ,builder)
+              (*builder* ,builder-var))
+         (flet ((,explicit-function (&rest args)
+                  (apply ',implicit-function (rest args))))
+           ,@body)))))
+
 ;;; Global processing tests
 
 (test prepare.smoke
-  "Smoke test for the `prepare' generic function."
+  "Smoke test for the `prepare[*]' functions."
 
-  (let ((builder (prepare (make-instance 'preparable-mock-builder))))
-    (is (equalp (mock-node :foo '(:prepared? t))
-                (make-node builder :foo)))))
+  (with-implicit-and-explicit-builder
+      (builder (make-instance 'preparable-mock-builder))
+      prepare
+    (let ((builder (prepare builder)))
+      (is (equalp (mock-node :foo '(:prepared? t))
+                  (make-node builder :foo))))))
 
 (test finish.smoke
-  "Smoke test for the `finish' generic function."
+  "Smoke test for the `finish[*]' functions."
 
-  (let ((builder (prepare (make-instance 'finish-mock-builder))))
+  (with-implicit-and-explicit-builder
+      (builder (prepare (make-instance 'finish-mock-builder)))
+      finish
     (is (equalp `(:finish ,(mock-node :foo))
                 (finish builder (make-node builder :foo))))))
 
@@ -28,30 +48,33 @@
   (make-node builder :foo))
 
 (test wrap.smoke
-  "Smoke test for the `wrap' generic function."
+  "Smoke test for the `wrap[*]' functions."
 
   (macrolet ((test-case (wrapper)
-               `(let ((builder (make-instance 'mock-builder)))
-                  (is (equalp (mock-node :foo)
-                              (wrap builder ,wrapper))))))
+               `(with-implicit-and-explicit-builder
+                    (builder (make-instance 'mock-builder))
+                    wrap
+                  (is (equalp (mock-node :foo) (wrap builder ,wrapper))))))
     (test-case 'a-wrapper)
     (test-case #'a-wrapper)))
 
 ;;; Node construction tests
 
 (test make-node.smoke
-  "Smoke test for the `make-node' generic function."
+  "Smoke test for the `make-node[*]' functions."
 
-  (let ((builder (make-instance 'mock-builder)))
+  (with-implicit-and-explicit-builder (builder (make-instance 'mock-builder))
+      make-node
     (is (equalp (mock-node :foo)
                 (make-node builder :foo)))
     (is (equalp (mock-node :foo '(:bar 1))
                 (make-node builder :foo :bar 1)))))
 
 (test relate.smoke
-  "Smoke test for `relate' generic function."
+  "Smoke test for `relate[*]' functions."
 
-  (let ((builder (make-instance 'mock-builder)))
+  (with-implicit-and-explicit-builder (builder (make-instance 'mock-builder))
+      relate
     (is (equalp (mock-node :foo () `((:baz . ((,(mock-node :bar) . nil)))))
                 (relate builder :baz (mock-node :foo) (mock-node :bar))))
     (is (equalp (mock-node :foo () `((:baz . ((,(mock-node :bar) . (:fez 2))))))
