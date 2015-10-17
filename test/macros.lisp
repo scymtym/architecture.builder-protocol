9a52c7a78169626bcc55f7fba476ad1527d6f599
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-10-17 20:10:42 +0200

    Added tests for node[*] macros in test/macros.lisp
    * test/macros.lisp (node.smoke): new test; for `node' macro
      (node*.smoke): new test; for `node*' macro

--- test/macros.lisp
+++ test/macros.lisp
@@ -6,6 +6,8 @@
 
 (cl:in-package #:architecture.builder-protocol.test)
 
+;;; `with-builder'
+
 (test with-builder.smoke.1
   "Smoke test for first syntax of the `with-builder' macro."
 
@@ -21,3 +23,81 @@
                                         'preparable-finish-mock-builder))
                   (make-node builder :foo))))
     (is (equalp `(:finish ,(mock-node :foo :slots '(:prepared? t))) result))))
+
+;;; `node[*]'
+
+(test node.smoke
+  "Smoke test for the `node' macro."
+
+  (with-builder (builder (make-instance 'mock-builder))
+    ;; Bare minimum.
+    (is (equalp (mock-node :foo :finished? t)
+                (node (builder :foo))))
+    ;; Initargs.
+    (is (equalp (mock-node :foo :slots '(:bar 1) :finished? t)
+                (node (builder :foo :bar 1))))
+    ;; Relation with ? cardinality.
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t))))
+                           :finished? t)
+                (node (builder :foo)
+                  (? :bar (node (builder :baz))))))
+    ;; Relation with 1 cardinality.
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t))))
+                           :finished? t)
+                (node (builder :foo)
+                  (1 :bar (node (builder :baz))))))
+    ;; Relation with * cardinality.
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t))
+                                              (,(mock-node :baz :finished? t))))
+                           :finished? t)
+                (node (builder :foo)
+                  (* :bar (list (node (builder :baz))
+                                (node (builder :baz)))))))
+    ;; Relation arguments
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t)
+                                               :fez 1)))
+                           :finished? t)
+                (node (builder :foo)
+                  (? :bar (node (builder :baz)) :fez 1))))))
+
+(test node*.smoke
+  "Smoke test for the `node*' macro."
+
+  (with-builder ((make-instance 'mock-builder))
+    ;; Bare minimum.
+    (is (equalp (mock-node :foo :finished? t)
+                (node* (:foo))))
+    ;; Initargs.
+    (is (equalp (mock-node :foo :slots '(:bar 1) :finished? t)
+                (node* (:foo :bar 1))))
+    ;; Relation with ? cardinality.
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t))))
+                           :finished? t)
+                (node* (:foo)
+                  (? :bar (node* (:baz))))))
+    ;; Relation with 1 cardinality.
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t))))
+                           :finished? t)
+                (node* (:foo)
+                  (1 :bar (node* (:baz))))))
+    ;; Relation with * cardinality.
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t))
+                                              (,(mock-node :baz :finished? t))))
+                           :finished? t)
+                (node* (:foo)
+                  (* :bar (list (node* (:baz))
+                                (node* (:baz)))))))
+    ;; Relation arguments
+    (is (equalp (mock-node :foo
+                           :relations `((:bar (,(mock-node :baz :finished? t)
+                                                :fez 1)))
+                           :finished? t)
+                (node* (:foo)
+                  (? :bar (node* (:baz)) :fez 1))))))
