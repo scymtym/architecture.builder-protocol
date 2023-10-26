07ff344da0140ea43f10ac409aec85de7afa3c9b
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2023-10-26 17:10:51 +0200

    Remove initial value of *BUILDER*
    The initial value NIL makes debugging harder and provides little
    value.
    
    Suggested-by: Robert Strandh

--- src/variables.lisp
+++ src/variables.lisp
@@ -1,11 +1,12 @@
 ;;;; variables.lisp --- Variables provided by the architecture.builder-protocol system.
 ;;;;
-;;;; Copyright (C) 2014, 2015 Jan Moringen
+;;;; Copyright (C) 2014-2023 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:architecture.builder-protocol)
 
-(defvar *builder* nil
-  "Stores the builder object which should be used for constructing the
-   result.")
+(defvar *builder*)
+(setf (documentation '*builder* 'variable)
+      "Stores the builder object which should be used for constructing the
+result.")
